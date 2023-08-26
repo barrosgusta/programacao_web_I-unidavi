099923ffe6ead5326350d45881533c64e18a597c
barrosgusta <59765082+barrosgusta@users.noreply.github.com>
2023-08-26 14:57:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const NUMBER_OF_EXERCISES_LIST_1 = 21;
 
 function fillTable() {
+  let contentToBeAdded = "";
   //Preenche a tabela com os exercicios da lista 1
   for (let i = 1; i <= NUMBER_OF_EXERCISES_LIST_1; i++) {
-    document.getElementById("main-table").innerHTML += 
-      `<tr onclick="openUrl('unidavi/programacao_web_I/exercicios/lista-1/ex${i < 10 ? "0" + i : i}/index.html')" class="bg-zinc-100 cursor-pointer hover:bg-zinc-400 hover:text-white duration-100">
-      <td>
-          Lista 1 - Exercicio ${i}
-      </td>
+    contentToBeAdded += 
+      `<tr onclick="openUrl('unidavi/programacao_web_I/exercicios/lista-1/ex${i < 10 ? "0" + i : i}/index.html')" class="bg-zinc-100 cursor-pointer hover:bg-zinc-400 hover:text-white duration-100 group"}">
+        <td class="group-last:rounded-bl-md">
+            Lista 1 - Exercício ${i}
+        </td>
       </tr>`;
   }
+  document.getElementById("main-table").innerHTML += contentToBeAdded;
+
 
   return true;
 }
