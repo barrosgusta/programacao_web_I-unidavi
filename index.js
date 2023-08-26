1802ae20354b90efeb992253d27213519e4cc0df
barrosgusta <59765082+barrosgusta@users.noreply.github.com>
2023-08-26 11:44:51 -0300

    dynamically add the table rows

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+const NUMBER_OF_EXERCISES_LIST_1 = 21;
+
+function fillTable() {
+  //Preenche a tabela com os exercicios da lista 1
+  for (let i = 1; i <= NUMBER_OF_EXERCISES_LIST_1; i++) {
+    document.getElementById("main-table").innerHTML += 
+      `<tr onclick="openUrl('unidavi/programacao_web_I/exercicios/lista-1/ex${i < 10 ? "0" + i : i}/index.html')" class="bg-zinc-100 cursor-pointer hover:bg-zinc-400 hover:text-white duration-100">
+      <td>
+          Lista 1 - Exercicio ${i}
+      </td>
+      </tr>`;
+  }
+
+  return true;
+}
+
+window.onload = () => {
+  console.log(fillTable() && "Tabela preenchida com sucesso! - Lista 1");
+}
+
 function openUrl(url) {
   window.open(url);
 }
