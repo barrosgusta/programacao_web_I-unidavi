a768f1c54805dfc37cd2a96d8dc7c1082a2a95cf
barrosgusta <59765082+barrosgusta@users.noreply.github.com>
2023-09-13 14:54:39 -0300

    change the front

--- index.js
+++ index.js
@@ -1,24 +1,45 @@
 const NUMBER_OF_EXERCISES_LIST_1 = 21;
+const NUMBER_OF_EXERCISES_LIST_2 = 1;
 
-function fillTable() {
-  let contentToBeAdded = "";
-  //Preenche a tabela com os exercicios da lista 1
+function fillHTMLList(content) {
   for (let i = 1; i <= NUMBER_OF_EXERCISES_LIST_1; i++) {
-    contentToBeAdded += 
-      `<tr onclick="openUrl('unidavi/programacao_web_I/exercicios/lista-1/ex${i < 10 ? "0" + i : i}/index.html')" class="dark:bg-zinc-800 dark:text-zinc-200 b bg-zinc-100 cursor-pointer hover:bg-zinc-400 hover:text-white duration-100 group"}">
+    content.toBeAdded +=
+     `<tr onclick="openUrl('unidavi/programacao_web_I/exercicios/lista-1/ex${i < 10 ? "0" + i : i}/index.html')" class="dark:bg-zinc-800 dark:text-zinc-200 b bg-zinc-100 cursor-pointer hover:bg-zinc-400 hover:text-white duration-100 group"}">
         <td class="p-1 border-b border-r border-zinc-600 group-last:rounded-bl-md group-last:border-b-0">
-            Lista 1 - Exercício ${i}
+            HTML | Exercício ${i}
         </td>
       </tr>`;
   }
-  document.getElementById("main-table").innerHTML += contentToBeAdded;
+}
 
+  function fillCSSList(content) {
+    for (let i = 1; i <= NUMBER_OF_EXERCISES_LIST_2; i++) {
+      content.toBeAdded +=
+        `<tr onclick="openUrl('unidavi/programacao_web_I/exercicios/lista-2/ex${i < 10 ? "0" + i : i}/index.html')" class="dark:bg-zinc-800 dark:text-zinc-200 b bg-zinc-100 cursor-pointer hover:bg-zinc-400 hover:text-white duration-100 group"}">
+          <td class="p-1 border-b border-r border-zinc-600 group-last:rounded-bl-md group-last:border-b-0">
+              CSS | Exercício ${i}
+          </td>
+        </tr>`;
+    }
+  }
+  
+
+
+function fillTable() {
+  let content = { toBeAdded: "" };
+  //Preenche a tabela com os exercicios da lista 1
+  fillHTMLList(content);
+  //Preenche a tabela com os exercicios da lista 2
+  fillCSSList(content);
+  console.log(content.toBeAdded);
+
+  document.getElementById("main-table").innerHTML += content.toBeAdded;
 
   return true;
 }
 
 window.onload = () => {
-  console.log(fillTable() && "Tabela preenchida com sucesso! - Lista 1");
+  console.log(fillTable() && "Tabela preenchida com sucesso!");
 }
 
 function openUrl(url) {
